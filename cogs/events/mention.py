b976037dbdf63a525f17ac6d484b9d4b8cae7e8c
reach1234889 <reachreaven4@gmail.com>
2025-06-29 10:43:20 +0800

    Update mention.py

--- cogs/events/mention.py
+++ cogs/events/mention.py
@@ -55,7 +55,7 @@ class Mention(commands.Cog):
                 embed.set_footer(text="Powered by Olympus Development™", icon_url=self.bot.user.avatar.url)
 
                 buttons = [
-                    discord.ui.Button(label="Invite", style=discord.ButtonStyle.link, url="https://discord.com/oauth2/authorize?client_id=1144179659735572640&permissions=2113268958&scope=bot"),
+                    discord.ui.Button(label="Invite", style=discord.ButtonStyle.link, url="https://discord.com/oauth2/authorize?client_id=1387495824195321938&permissions=8&integration_type=0&scope=bot"),
                     discord.ui.Button(label="Web", style=discord.ButtonStyle.link, url="https://olyumpus.vercel.app/"),
                     discord.ui.Button(label="Support", style=discord.ButtonStyle.link, url="https://discord.gg/SQznKsJyDd"),
                 ]
