f5fe120392db0e4238308a2dc86e3491a238a176
reach1234889 <reachreaven4@gmail.com>
2025-06-29 10:24:37 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "zyronbot"
+NAME = "zyron"
 server = "https://discord.gg/SQznKsJyDd"
 ch = "https://discord.com/channels/1378349908427538583/1378349909102559330"
 OWNER_IDS = [875671806609084428, 875671806609084428]
