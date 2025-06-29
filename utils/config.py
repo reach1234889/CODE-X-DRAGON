5c2c882ac22a3c5a892aa1fefe377963a75c4ca3
reach1234889 <reachreaven4@gmail.com>
2025-06-29 10:22:29 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Olympus"
-server = "https://discord.com/invite/odx"
-ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
-BotName = "Olympus"
-serverLink = "https://discord.com/invite/odx"
+NAME = "zyronbot"
+server = "https://discord.gg/SQznKsJyDd"
+ch = "https://discord.com/channels/1378349908427538583/1378349909102559330"
+OWNER_IDS = [875671806609084428, 875671806609084428]
+BotName = "zyronbot"
+serverLink = "https://discord.gg/SQznKsJyDd"
